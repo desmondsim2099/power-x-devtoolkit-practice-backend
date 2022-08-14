e1665a6138f8660431c56b242a502b2ecd6b4a02
Desmond Sim <ega.desmond@gmail.com>
2022-08-14 22:08:21 +0800

    Added Post Requests

--- customer.js
+++ customer.js
@@ -15,16 +15,31 @@ router.get("/customer/all", (request, response) => {
     }
   });
 });
-//Note: use query instead of all for MySQL - database.connection.query("select * from customer"
+// //Note: use query instead of all for MySQL - database.connection.query("select * from customer"
 
-// defines an API which takes id in the request and return the record in response
-router.get("/customer/id", (request, response) => {
-  sqlst = `select * from customer where id = "${request.query.cid}"`; 
+// // defines an API which takes id in the request and return the record in response
+// router.get("/customer/id", (request, response) => {
+//   sqlst = `select * from customer where id = "${request.query.cid}"`; 
+//   database.connection.all(
+//     sqlst,
+//     (errors, results) => {
+//       if (errors) {
+//         response.status(500).send("Some error occurred" + sqlst);
+//       } else {
+//         response.status(200).send(results);
+//       }
+//     }
+//   );
+// });
+
+// a POST API to store the record received in the request
+router.post("/customer/add", (request, response) => {
   database.connection.all(
-    sqlst,
+    `select id , itemID
+    FROM shop_order WHERE itemID IN (SELECT id FROM item WHERE price <= ${request.body.name});`,
     (errors, results) => {
       if (errors) {
-        response.status(500).send("Some error occurred" + sqlst);
+        response.status(500).send("Some error occurred");
       } else {
         response.status(200).send(results);
       }
@@ -32,38 +47,25 @@ router.get("/customer/id", (request, response) => {
   );
 });
 
-// a POST API to store the record received in the request
-router.post("/customer/add", (request, response) => {
-  database.connection.all(
-    `insert into customer (customer_name, customer_email) values ('${request.body.name}','${request.body.email}')`,
-    (errors, results) => {
-      if (errors) {
-        response.status(500).send("Some error occurred");
-      } else {
-        response.status(200).send("Record saved successfully!");
-      }
-    }
-  );
-});
-
-// POST + PUT = Body, GET + DELETE = Query
-router.delete("/customer/delete", (request, response) => {
-  database.connection.all(
-    `delete from customer where customer_id  = ${request.query.cid}`,
-    (errors, results) => {
-      if (errors) {
-        response.status(500).send("Some error occurred");
-      } else {
-        response.status(200).send("Record deleted successfully!");
-      }
-    }
-  );
-});
-
+// // POST + PUT = Body, GET + DELETE = Query
+// router.delete("/customer/delete", (request, response) => {
+//   database.connection.all(
+//     `delete from customer where customer_id  = ${request.query.cid}`,
+//     (errors, results) => {
+//       if (errors) {
+//         response.status(500).send("Some error occurred");
+//       } else {
+//         response.status(200).send("Record deleted successfully!");
+//       }
+//     }
+//   );
+// });
+// ${request.body.cid}
 // a PUT API to update email for given customer id
-router.put("/customer/change", (request, response) => {
-  sqlstmt = `UPDATE customer SET customer_email = "${request.body.cemail}"
-    WHERE customer_id  = ${request.body.cid}`;
+router.post("/customer/change", (request, response) => {
+  value = request.body.cid
+  sqlstmt = `SELECT customer.id , customer.name , shop_order.id FROM
+  customer , shop_order WHERE customer.id = shop_order.customerID AND shop_order.customerID = "${value}";`;
   
   database.connection.all(
    sqlstmt,
@@ -71,7 +73,8 @@ router.put("/customer/change", (request, response) => {
       if (errors) {
         response.status(500).send("Some error occurred" + sqlstmt);
       } else {
-        response.status(200).send("Record updated successfully!" + sqlstmt);
+        console.log(sqlstmt)
+        response.status(200).send(results);
       }
     }
   );
